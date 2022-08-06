9373ebe7c40786c01ad3b5931a72f3e32f79685e
abiwinanda <abiwinanda@outlook.com>
2022-08-06 16:56:28 +0800

    new project

---
+++ lib/ecto_embedded_schema.ex
@@ -0,0 +1,18 @@
+defmodule EctoEmbeddedSchema do
+  @moduledoc """
+  Documentation for `EctoEmbeddedSchema`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoEmbeddedSchema.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_embedded_schema_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoEmbeddedSchemaTest do
+  use ExUnit.Case
+  doctest EctoEmbeddedSchema
+
+  test "greets the world" do
+    assert EctoEmbeddedSchema.hello() == :world
+  end
+end
